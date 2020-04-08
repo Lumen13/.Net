c2558c14719c6b739168c998c986ff3c527a908b
lumen13 <ntap13@gmail.com>
2020-04-08 15:26:14 +0300

    .Up

--- TechAtHome/Startup.cs
+++ TechAtHome/Startup.cs
@@ -14,8 +14,7 @@ namespace TechAtHome
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IGoods, MockGoods>();
@@ -23,8 +22,7 @@ namespace TechAtHome
             //services.AddControllersWithViews();
             services.AddMvc();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
